8d7a077d3cbf1310318fda93b2f4e08138f26880
Valéria Calado <valeriacalado7@gmail.com>
2023-06-03 18:07:21 -0300

    Adding the database and table creating files

---
+++ [2] creating tables.sql
@@ -0,0 +1,24 @@
+use braziliancities;
+
+create table region (
+	region_id INT NOT NULL AUTO_INCREMENT,
+    region VARCHAR(255) NOT NULL,
+    primary key (region_id)
+	);
+
+create table state (
+	state_id INT NOT NULL AUTO_INCREMENT,
+    state VARCHAR(100) NOT NULL,
+    region INT NOT NULL,
+    primary key (state_id),
+    foreign key (region) references region(region_id)
+	);
+
+create table cities (
+	city_id INT NOT NULL AUTO_INCREMENT,
+    city VARCHAR(255) NOT NULL,
+    state INT NOT NULL,
+    primary key (city_id),
+    foreign key (state) references state(state_id)
+    )
+
